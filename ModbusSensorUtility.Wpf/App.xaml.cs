2dbbec75a9af7929f1c1c7c5c0dae915832cacb8
SergeyFM <github@svistunov.su>
2023-07-30 21:26:54 +0300

    Add project files.

---
+++ ModbusSensorUtility.Wpf/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using ModbusSensorUtility.Log;
+
+namespace ModbusSensorUtility.Wpf;
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application {
+    protected override void OnStartup(StartupEventArgs e) {
+        base.OnStartup(e);
+        var logger = LogService.Instance;
+        logger.Log("The App starts...");
+    }
+}

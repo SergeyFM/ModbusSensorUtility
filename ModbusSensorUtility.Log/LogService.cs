e983cc4e898246aa756c0ff14dcf15872be341d1
SergeyFM <github@svistunov.su>
2023-07-31 21:37:18 +0300

    Add a Log project

---
+++ ModbusSensorUtility.Log/LogService.cs
@@ -0,0 +1,17 @@
+﻿namespace ModbusSensorUtility.Log;
+public sealed class LogService {
+
+    private static readonly Lazy<LogService> lazy = new(() => new LogService());
+
+    public static LogService Instance => lazy.Value;
+
+    private LogService() { }
+
+    public void Log(string message) {
+
+        string stamp = DateTime.Now.ToString("yyyyMMdd-HH:mm:ss:ffff");
+        Console.WriteLine($"{stamp} message");
+
+    }
+
+}
